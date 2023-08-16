a5594ccb607c9837066e3dfe90f6e0b5035b7535
davidkonrad <davidkonrad@gmail.com>
2023-08-16 19:38:11 +0200

    added .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+		"env": {
+        "browser": true,
+				"node": true,
+        "es2020": true
+    },
+		"root": true,
+    "extends": "eslint:recommended",
+    "globals": {
+    },
+    "parserOptions": {
+        "ecmaVersion": "2020",
+        "sourceType": "module"
+    },
+    "rules": {
+        "eqeqeq": "error"
+    }
+};
